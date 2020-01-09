83c644e40d438048d301dc4435f27b3a13432667
Brendan Chapman <55817314+ChappyGames@users.noreply.github.com>
2020-01-08 21:32:01 -0600

    Fixed Premake5 file
    Fixed Premake5 file

--- premake5.lua
+++ premake5.lua
@@ -1,114 +1,42 @@
-workspace "ChappyEngine"
-	architecture "x64"
+project "GLFW"
+	kind "StaticLib"
+	language "C"
 	
-	configurations {
-		"Debug",
-		"Release",
-		"Dist"
-	}
-	
-outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-
--- Include directories relative to root folder (solution directory)
-IncludeDir = {}
-IncludeDir["GLFW"] = "ChappyEngine/vendors/GLFW/include"
-
-include "ChappyEngine/vendors/GLFW"
-	
-project "ChappyEngine"
-	location "ChappyEngine"
-	kind "SharedLib"
-	language "C++"
-	
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-	
-	pchheader "cepch.h"
-	pchsource "ChappyEngine/src/cepch.cpp"
+	targetdir("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
 	
 	files {
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
-		
-	}
-	
-	includedirs {
-		"%{prj.name}/src",
-		"%{prj.name}/vendors/spdlog/include",
-		"%{IncludeDir.GLFW}"
-	}
-	
-	links {
-		"GLFW",
-		"opengl32.lib"
+		"include/GLFW/glfw3.h",
+		"include/GLFW/glfw3native.h",
+		"src/glfw_config.h",
+		"src/context.c",
+		"src/init.c",
+		"src/input.c",
+		"src/monitor.c",
+		"src/vulkan.c",
+		"src/window.c"
 	}
 	
 	filter "system:windows"
-		cppdialect "C++17"
 		staticruntime "On"
 		systemversion "latest"
-		
-		defines {
-			"CE_PLATFORM_WINDOWS",
-			"CE_BUILD_DLL"
-		}
-		
-		postbuildcommands {
-			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
-		}
-		
-	filter "configurations:Debug"
-		defines "CE_DEBUG"
-		symbols "On"
-		
-	filter "configurations:Release"
-		defines "CE_RELEASE"
-		optimize "On"
-		
-	filter "configurations:Dist"
-		defines "CE_DIST"
-		optimize "On"
-	
-project "Sandbox"
-	location "Sandbox"
-	kind "ConsoleApp"
-	language "C++"
-	
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 	
 	files {
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
-		
+		"src/win32_init.c",
+		"src/win32_joystick.c",
+		"src/win32_monitor.c",
+		"src/win32_time.c",
+		"src/win32_thread.c",
+		"src/win32_window.c",
+		"src/wgl_context.c",
+		"src/egl_context.c",
+		"src/osmesa_context.c"
 	}
 	
-	includedirs {
-		"ChappyEngine/vendors/spdlog/include",
-		"ChappyEngine/src"
+	defines {
+		"_GLFW_WIN32",
+		"_CRT_SECURE_NO_WARNINGS"
 	}
 	
-	links {
-		"ChappyEngine"
-	}
-	
-	filter "system:windows"
-		cppdialect "C++17"
-		staticruntime "On"
-		systemversion "latest"
-		
-		defines {
-			"CE_PLATFORM_WINDOWS"
-		}
-		
-	filter "configurations:Debug"
-		defines "CE_DEBUG"
-		symbols "On"
-		
-	filter "configurations:Release"
-		defines "CE_RELEASE"
-		optimize "On"
-		
-	filter "configurations:Dist"
-		defines "CE_DIST"
-		optimize "On"
+	filter { "system:windows", "configurations:Release" }
+		buildoptions "/MT"
